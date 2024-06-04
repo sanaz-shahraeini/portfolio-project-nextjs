7134aa6feedf3fb1228369ffc4dec849f2b17e96
Sanaz <s.shahraeini@gmail.com>
2024-06-04 14:57:59 +0330

    delete about.css

--- components/About.tsx
+++ components/About.tsx
@@ -5,8 +5,7 @@ import { motion } from "framer-motion";
 import { services } from "@/data";
 import { SectionWrapper } from "../hoc/index";
 import { fadeIn } from "../lib/motion";
-import Image from "next/image";
-import "./about.css";
+
 
 interface IServiceCard {
   index: number;
@@ -19,13 +18,13 @@ const ServiceCard: React.FC<IServiceCard> = ({ index, title, icon }) => (
     <div className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="border-2 background-card w-full rounded-[20px] p-[1px]"
+        className="border-2 w-full rounded-[20px] p-[1px]"
         style={{
-          //   add these two
-          //   you can generate the color from here https://cssgradient.io/
+ 
           background: "rgb(4,7,29)",
-          backgroundColor:
-            "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+            borderColor: "rgb(84, 5, 137)",
+         
         }}
       >
         <div className="flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] px-12 py-5">
@@ -52,7 +51,7 @@ const About = () => {
         Who <span className="text-purple">I am?</span>
       </h1>
      
-        <p className=" text-white-100 my-10 font-bold text-[20px] text-center">
+        <p className="text-white-100 my-10 text-[20px] text-center">
           I'm a skilled web developer with experience in TypeScript &
           JavaScript, and expertise in frameworks like React, Next.js &
           Three.js.
