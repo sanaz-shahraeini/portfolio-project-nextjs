38575672c9655c445f46cdf2852081dd5c8e364f
Sanaz <s.shahraeini@gmail.com>
2024-10-13 00:05:38 +0330

    optimize favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-      <link rel="icon" href="/favicon.ico"  />
-      </head>
+    
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
